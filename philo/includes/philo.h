476b3852dcf25273d9f1d73326686cf3aa94254a
elsa2491 <elsa.touma@gmail.com>
2024-06-21 18:57:41 +0200

    Initial commit

---
+++ philo/includes/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 18:37:27 by eltouma           #+#    #+#             */
+/*   Updated: 2024/06/21 18:39:44 by eltouma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include "structures.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+
+#endif
---
+++ philo/includes/structures.h
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structures.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 16:22:29 by eltouma           #+#    #+#             */
+/*   Updated: 2024/06/21 18:55:04 by eltouma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURES_H
+# define STRUCTURES_H
+
+typedef enum e_all_states
+{
+	EAT = 1,
+	SLEEP,
+	THINK,
+	DEAD,
+}	t_all_states;
+
+typedef struct s_state
+{
+	t_all_states	type;
+	//	pthread_mutex_t	state_mutex;
+	struct s_state	*next;
+	struct s_state	*prev;
+}	t_state;
+
+typedef struct s_fork
+{
+	int	i;
+	pthread_mutex_t	fork_mutex;
+}	t_fork;
+
+typedef struct s_philo
+{
+	pthread_t	philo;
+	t_state		*state;
+	t_fork		*right_f;
+	t_fork		*left_f;
+	pthread_mutex_t	philo_mutex;
+	long		time_to_eat;
+	long		time_to_think;
+	long		time_to_sleep;
+	int		is_die;
+	int		i;
+	struct s_philo	*next;
+	struct s_philo	*prev;
+}	t_philo;
+
+typedef struct s_data
+{
+	t_philo		*philo;
+	t_state		*state;
+	t_fork		*right_f;
+	t_fork		*left_f;
+	pthread_mutex_t	data_mutex;
+}	t_data;
+
+#endif
