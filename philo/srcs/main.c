0c153b656e37860604a471b602031dc3a2f3dbc5
elsa2491 <elsa.touma@gmail.com>
2024-06-25 11:45:23 +0200

    [25.06 Beginning] Init data

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 18:40:49 by eltouma           #+#    #+#             */
-/*   Updated: 2024/06/25 10:35:19 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/06/25 11:45:00 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_init_data(t_data *data, char **argv)
 	data->fork_nb = ft_atoi(argv[1]);
 	data->time_before_dying = ft_atoi(argv[2]);
 	data->time_to_eat = ft_atoi(argv[3]);
-	data->time_to_sleep = ft_atoi(argv[3]);
-	dprintf(2, "Philo num: %d\n", data->philo_nb);
-	dprintf(2, "Fork num: %d\n", data->philo_nb);
-	dprintf(2, "Time before dying: %d\n", data->time_before_dying);
-	dprintf(2, "Time to eat: %d\n", data->time_to_eat);
-	dprintf(2, "Time to sleep: %d\n", data->time_to_sleep);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	dprintf(2, " Philo num:\t\t%d\n", data->philo_nb);
+	dprintf(2, " Fork num:\t\t%d\n", data->philo_nb);
+	dprintf(2, " Time before dying:\t%d\n", data->time_before_dying);
+	dprintf(2, " Time to eat:\t\t%d\n", data->time_to_eat);
+	dprintf(2, " Time to sleep:\t\t%d\n", data->time_to_sleep);
 }
 
 int	main(int argc, char **argv)
