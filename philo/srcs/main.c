ace38c3c81cee5cad64514ea8e3a8dd2a45fc083
elsa2491 <elsa.touma@gmail.com>
2024-06-27 21:23:25 +0200

    [27.06 Test ft_routine] No segfault si valeur de usleep est en brute

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,28 +6,30 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 18:40:49 by eltouma           #+#    #+#             */
-/*   Updated: 2024/06/27 19:19:48 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/06/27 21:21:40 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	ft_take_forks(t_philo *philo)
+void	ft_take_forks(t_philo **philo)
 {
 	dprintf(2, "\t->%s\n", __func__);
-	if (pthread_mutex_lock(philo->left_f) == 0)
+	if (pthread_mutex_lock((*philo)->left_f) == 0)
 		printf("j'ai pris ma fourchette gauche\n");
 	else // si la fourchette est occupee, je ne rentre pas dans le else parce que pthread ne retourne rien dans ce cas la, donc je reste coincee sur cette meme ligne
 		printf("error\n");
-	if (pthread_mutex_lock(philo->right_f) == 0)
+	if (pthread_mutex_lock((*philo)->right_f) == 0)
 		printf("j'ai pris ma fourchette right\n");
 	else
 		printf("error\n");
 	printf("je commence a manger\n");
-	usleep(philo->table->time_to_eat);
-	if (pthread_mutex_unlock(philo->right_f) == 0)
+	usleep(300000);
+//	dprintf(2, "time_to_eat %d\n", (*philo)->table->time_to_eat);
+//	usleep((*philo)->table->time_to_eat);
+	if (pthread_mutex_unlock((*philo)->right_f) == 0)
 		printf("j'ai repose ma fourchette droite\n");
-	if (pthread_mutex_unlock(philo->left_f) == 0)
+	if (pthread_mutex_unlock((*philo)->left_f) == 0)
 		printf("j'ai repose ma fourchette gauche\n");
 
 }
@@ -35,6 +37,7 @@ void	ft_take_forks(t_philo *philo)
 void	*ft_routine(void *philo)
 {
 	dprintf(2, "\t->%s\n", __func__);
+//	int	i = 0;
 	while (1)
 	{
 		ft_take_forks(philo);
@@ -42,6 +45,7 @@ void	*ft_routine(void *philo)
 //		ft_drop_forks();
 //		ft_sleep();
 //		ft_think();
+	//	i += 1;
 	}
 }
 
