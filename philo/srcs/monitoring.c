4562135a60ef99ae1861d5bc798e47f3b39f18eb
elsa2491 <elsa.touma@gmail.com>
2024-07-17 18:25:33 +0200

    [17.07 Norme] Split functions

---
+++ philo/srcs/monitoring.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitoring.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 18:40:49 by eltouma           #+#    #+#             */
+/*   Updated: 2024/07/17 15:38:54 by eltouma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+
+int	ft_is_dead(t_table *table, t_philo **philo, size_t dead)
+{
+	pthread_mutex_lock(&table->meal);
+	dead -= (*philo)->last_meal;
+	if (dead > table->time_before_dying)
+	{
+		pthread_mutex_unlock(&table->meal);
+		return (1);
+	}
+	pthread_mutex_unlock(&table->meal);
+	return (0);
+}
+
+int	ft_check_if_dead(t_table *table, t_philo **philo)
+{
+	int		i;
+	size_t	dead;
+
+	i = 0;
+	pthread_mutex_lock(&table->dead);
+	dead = ft_get_current_time();
+//	dprintf(2, "dead = %zu\n", dead);
+	while (i < table->philo_nb)
+	{
+		if (ft_is_dead(table, &philo[i], dead))
+		{
+			table->is_dead = 1;
+			pthread_mutex_unlock(&table->dead);
+			dprintf(2, "JE SUIS MORT\n");
+			return (1);
+		}
+		i += 1;
+	}
+		pthread_mutex_unlock(&table->dead);
+	return (0);
+}
+
+void	ft_monitoring(t_table *table, t_philo **philo)
+{
+	while (1)
+		if (ft_check_if_dead(table, philo) == 1)
+			break;
+}
---
+++ philo/srcs/thread_functions.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread_functions.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 18:40:49 by eltouma           #+#    #+#             */
+/*   Updated: 2024/07/17 17:17:29 by eltouma          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+static void	ft_init_mutexes(t_table *table)
+{
+	pthread_mutex_init(&table->message, NULL);
+	pthread_mutex_init(&table->dead, NULL);
+	pthread_mutex_init(&table->meal, NULL);
+}
+
+static void	ft_join_threads(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->philo_nb)
+	{
+		if (pthread_join(table->thread_id[i], NULL) != 0)
+			return ;
+		i += 1;
+	}
+}
+
+void	ft_init_threads(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	table->thread_id = (pthread_t *)malloc(sizeof(pthread_t) * table->philo_nb);
+	if (!table->thread_id)
+		return ;
+	// Mutex pour attendre la creation des threads
+	ft_init_mutexes(table);
+	while (i < table->philo_nb)
+	{
+		if (pthread_create(&(table->thread_id[i]), NULL, &ft_routine, &(table->philo_tab[i])) != 0)
+		{
+			dprintf(2, "Attention tout le monde, je fail %d!\n", i);
+		// Attention, si le thread 3 fail, il faut join les threads crees et tout arreter
+			return ;
+		}
+		i += 1;
+	}
+	// Unlock mutex 
+	ft_monitoring(table, table->philo_tab);
+	ft_join_threads(table);
+}
